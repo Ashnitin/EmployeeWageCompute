9a0bd0d450f16b9d4884ecc7db79a2ad13869f71
unknown <ashwinijadhav129aj@gmail.com>
2023-11-02 18:10:16 +0530

    Added: check Employee present or not

--- src/com/bridglabz/EmployeeWage.java
+++ src/com/bridglabz/EmployeeWage.java
@@ -2,6 +2,15 @@ package com.bridglabz;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        System.out.println("Welcome to emp computation");
+        //Constants
+        int IS_FULL_TIME=1;
+        //Computation
+        double empCheck = Math.floor(Math.random()*10)%2;
+        if (empCheck == IS_FULL_TIME){
+            System.out.println("Employee is Present");
+        }
+        else{
+            System.out.println("Employee is absent");
+        }
     }
 }
