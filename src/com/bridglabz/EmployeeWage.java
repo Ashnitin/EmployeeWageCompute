7cad0a7b4eae22951d9ab927d04027b783c3155d
unknown <ashwinijadhav129aj@gmail.com>
2023-11-03 10:52:52 +0530

    Added:UC3 Add Part time emp wage

--- src/com/bridglabz/EmployeeWage.java
+++ src/com/bridglabz/EmployeeWage.java
@@ -2,22 +2,26 @@ package com.bridglabz;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        //calculating employee wage
-        int IS_FULL_TIME = 1;
-        int EMP_RATE_PER_HOUR = 20;
+        //add part time employee and wages
+        //constant
+        int IS_PART_TIME=1;
+        int IS_FULL_TIME=2;
+        int EMP_RATE_PER_HOUR=20;
         //variable
         int empHrs = 0;
         int empWage = 0;
         //computation
-        double empCheck = Math.floor(Math.random()*10%2);
-        if(empCheck == IS_FULL_TIME){
+        double empCheck = Math.floor(Math.random()*10)%3;
+        if(empCheck == IS_PART_TIME){
+            empHrs = 4;
+        } else if (empCheck == IS_FULL_TIME) {
             empHrs=8;
         }
-        else{
+        else {
             empHrs=0;
         }
         empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Emp wage : "+empWage);
+        System.out.println("Emp Wages : "+empWage);
         
     }
 }
